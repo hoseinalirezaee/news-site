255b237044393794d2e7c678d334aec1cc84c93e
hoseinalirezaee <46540872+hoseinalirezaee@users.noreply.github.com>
2020-01-05 07:42:48 +0330

    News model created

---
+++ News/common/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class News(models.Model):
+
+    title = models.TextField(verbose_name='Title')
+    body = models.TextField(verbose_name='Body')
+    view_count = models.IntegerField(verbose_name='View Count')
+    root_category = models.CharField(verbose_name='Root Category', max_length=64)
+    category = models.CharField(verbose_name='Category', max_length=64)
+    image = models.TextField(verbose_name='Image')
+    publish_time = models.TimeField(verbose_name='Publish Time')
+    publish_date = models.DateField(verbose_name='Date')
+    lead = models.TextField(verbose_name='Lead Text')
+
+
+    class Meta:
+        verbose_name_plural = 'NEWS'
