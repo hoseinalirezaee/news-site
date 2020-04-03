31b4678041b98328d07889dd3bf403f755cbcf20
hoseinalirezaee <46540872+hoseinalirezaee@users.noreply.github.com>
2020-04-03 17:05:51 +0430

    mehrnews spider now uses rss feed

--- crawler/crawler/spiders/mehrnews.py
+++ crawler/crawler/spiders/mehrnews.py
@@ -3,42 +3,49 @@ import re
 import jdatetime
 from bs4 import BeautifulSoup
 from scrapy import Request
-from scrapy import Spider
+from scrapy.spiders import XMLFeedSpider
 
 from crawler.common.dateconvertor import month_str_to_int
 
+id_pattern = re.compile(r'/(\d+)/')
+category_pattern = re.compile(r'>(.+)$')
 
-class MehrnewsSpider(Spider):
+
+class MehrnewsSpider(XMLFeedSpider):
     name = 'mehrnews'
 
-    def start_requests(self):
-        for i in range(10):
-            yield Request(
-                f'https://www.mehrnews.com/page/archive.xhtml?pi={i}',
-                callback=self.parse_archive
-            )
+    start_urls = ['https://www.mehrnews.com/rss']
+    itertag = 'item'
 
-    def parse_archive(self, response):
-        links = response.xpath('//li[@class="news"]/.//a/@href').getall()
-        for link in links:
-            yield Request(
-                'https://www.mehrnews.com' + link,
-                callback=self.parse
-            )
+    # def start_requests(self):
+    #     for i in range(10):
+    #         yield Request(
+    #             f'https://www.mehrnews.com/page/archive.xhtml?pi={i}',
+    #             callback=self.parse_archive
+    #         )
 
-    def parse(self, response):
+    def parse_node(self, response, selector):
 
-        title = response.xpath('//h1[@class="title"]/a/text()').get()
-        summary = response.xpath('//p[@itemprop="description"]/text()').get()
-        main_image = response.xpath('//img[@itemprop="image"]/@src').get()
-        post_id = response.xpath('//div[@class="item-code"]/span/text()').get()
-        post_url = response.xpath('//div[@class="short-link-container"]/.//input/@value').get()
+        post_url = selector.xpath('link/text()').get()
+        raw_category = selector.xpath('category/text()').get()
+        data = dict()
+        data['title'] = selector.xpath('title/text()').get()
+        data['postUrl'] = post_url
+        data['summary'] = selector.xpath('description/text()').get()
+        data['mainImage'] = selector.xpath('enclosure/@url').get()
+        data['category'] = re.search(category_pattern, raw_category).group(1).strip()
+        data['dataTime'] = selector.xpath('pubDate/text()').get()
+        data['postId'] = re.search(id_pattern, post_url).group(1)
+
+        return Request(
+            data['postUrl'],
+            callback=self.parse_page,
+            cb_kwargs={'data': data}
+        )
+
+    def parse_page(self, response, data):
         tags = response.xpath('//section[contains(@class, "tag")]/.//a/text()').getall()
-        category = response.xpath('//div[@class="item-header"]/.//ol[@class="breadcrumb"]/.//a/text()').getall()[1]
-        date = response.xpath('//div[contains(@class, "item-date")]/span/text()').get()
-        datetime = MehrnewsSpider.str_to_date(date)
         paragraphs = []
-
         paragraphs_selector = response.xpath('//div[@itemprop="articleBody"]/p')
         for p in paragraphs_selector:
             image = p.xpath('.//img')
@@ -61,17 +68,65 @@ class MehrnewsSpider(Spider):
                         }
                     )
 
-        yield {
-            'title': title.strip(),
-            'summary': summary.strip(),
-            'dateTime': str(datetime),
-            'mainImage': main_image.strip(),
-            'paragraphs': paragraphs,
-            'postId': int(post_id),
-            'postUrl': post_url.strip(),
-            'tags': tags,
-            'category': category.strip()
-        }
+        data['paragraphs'] = paragraphs
+        data['tags'] = tags
+
+        yield data
+
+    # def parse_archive(self, response):
+    #     links = response.xpath('//li[@class="news"]/.//a/@href').getall()
+    #     for link in links:
+    #         yield Request(
+    #             'https://www.mehrnews.com' + link,
+    #             callback=self.parse
+    #         )
+
+    # def parse(self, response):
+    #
+    #     title = response.xpath('//h1[@class="title"]/a/text()').get()
+    #     summary = response.xpath('//p[@itemprop="description"]/text()').get()
+    #     main_image = response.xpath('//img[@itemprop="image"]/@src').get()
+    #     post_id = response.xpath('//div[@class="item-code"]/span/text()').get()
+    #     post_url = response.xpath('//div[@class="short-link-container"]/.//input/@value').get()
+    #     tags = response.xpath('//section[contains(@class, "tag")]/.//a/text()').getall()
+    #     category = response.xpath('//div[@class="item-header"]/.//ol[@class="breadcrumb"]/.//a/text()').getall()[1]
+    #     date = response.xpath('//div[contains(@class, "item-date")]/span/text()').get()
+    #     datetime = MehrnewsSpider.str_to_date(date)
+    #     paragraphs = []
+    #
+    #     paragraphs_selector = response.xpath('//div[@itemprop="articleBody"]/p')
+    #     for p in paragraphs_selector:
+    #         image = p.xpath('.//img')
+    #         if image:
+    #             image_url = image.xpath('./@src').get()
+    #             paragraphs.append(
+    #                 {
+    #                     'type': 'image',
+    #                     'data': image_url.strip()
+    #                 }
+    #             )
+    #         else:
+    #             bs = BeautifulSoup(p.get(), features='lxml')
+    #             text = bs.text
+    #             if text and text.strip():
+    #                 paragraphs.append(
+    #                     {
+    #                         'type': 'text',
+    #                         'data': text.strip()
+    #                     }
+    #                 )
+    #
+    #     yield {
+    #         'title': title.strip(),
+    #         'summary': summary.strip(),
+    #         'dateTime': str(datetime),
+    #         'mainImage': main_image.strip(),
+    #         'paragraphs': paragraphs,
+    #         'postId': int(post_id),
+    #         'postUrl': post_url.strip(),
+    #         'tags': tags,
+    #         'category': category.strip()
+    #     }
 
     @staticmethod
     def str_to_date(str):
