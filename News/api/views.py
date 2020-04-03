9e971bf74716650eb21972364414c6392c7deb38
hoseinalirezaee <46540872+hoseinalirezaee@users.noreply.github.com>
2020-04-03 22:11:11 +0430

    authentication added to post api

--- News/api/views.py
+++ News/api/views.py
@@ -1,13 +1,17 @@
+from rest_framework import authentication
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from . import permissions
 from . import serializers
 
 
 class PostViewSet(viewsets.ModelViewSet):
     http_method_names = ['post']
     serializer_class = serializers.PostSerializer
+    authentication_classes = [authentication.BasicAuthentication, authentication.SessionAuthentication]
+    permission_classes = [permissions.CanPost]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, many=True)
