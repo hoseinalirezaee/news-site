69f0595fdfa548c3fc36d16c2fac9580c352cfb7
Hosein Alirezaee <hoseinalirezaee128@gmail.com>
2020-04-19 18:52:37 +0430

    category mapper added

--- News/api/serializers.py
+++ News/api/serializers.py
@@ -2,24 +2,25 @@ from django.db import IntegrityError
 from rest_framework import serializers
 
 from db import models
+from .categorymapper import map_category
 
 
 class PostSerializer(serializers.Serializer):
     title = serializers.CharField()
     postUrl = serializers.URLField()
-    summary = serializers.CharField(allow_null=True)
-    mainImage = serializers.URLField(allow_null=True)
+    summary = serializers.CharField(allow_null=True, allow_blank=True)
+    mainImage = serializers.CharField(allow_null=True, allow_blank=True)
     category = serializers.CharField()
     dateTime = serializers.DateTimeField()
-    postId = serializers.IntegerField()
+    postId = serializers.RegexField(r'\d+', max_length=20, allow_blank=True, allow_null=True)
     paragraphs = serializers.JSONField()
     tags = serializers.ListField(child=serializers.CharField(), allow_null=True, allow_empty=True)
     agencyTitle = serializers.CharField()
     agencyCode = serializers.CharField()
 
     def create(self, validated_data):
-        category, created = models.Category.objects.get_or_create(
-            title=validated_data['category']
+        category = models.Category.objects.get(
+            title=map_category(validated_data['category'])
         )
 
         agency, created = models.Agency.objects.get_or_create(
@@ -41,19 +42,20 @@ class PostSerializer(serializers.Serializer):
 
         )
 
-        post.paragraphs = validated_data['paragraphs']
+        if created:
+            post.paragraphs = validated_data['paragraphs']
 
-        for tag in validated_data['tags']:
-            tag, created = models.Tag.objects.get_or_create(
-                title=tag
-            )
-            try:
-                models.PostTag.objects.create(
-                    post=post,
-                    tag=tag
+            for tag in validated_data['tags']:
+                tag, created = models.Tag.objects.get_or_create(
+                    title=tag
                 )
-            except IntegrityError:
-                pass
+                try:
+                    models.PostTag.objects.create(
+                        post=post,
+                        tag=tag
+                    )
+                except IntegrityError:
+                    pass
 
         post.save()
         return post
