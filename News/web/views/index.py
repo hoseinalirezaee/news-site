3eb3843e3b1015885644f9ec79d6c9cb41e2648a
Hosein Alirezaee <hoseinalirezaee128@gmail.com>
2020-06-19 11:24:05 +0430

    simplify queries

--- News/web/views/index.py
+++ News/web/views/index.py
@@ -16,17 +16,17 @@ class Index(generic.TemplateView):
         return context
 
     def get_last_news(self):
-        last_news_queryset = models.Post.objects.all().filter(main_image__isnull=False) \
+        last_news_queryset = models.Post.objects.filter(main_image__isnull=False) \
                                  .order_by('-date_posted')[:self.last_news_size]
         return last_news_queryset
 
     def get_last_news_with_category(self):
-        root_categories = models.Category.objects.all().filter(parent__isnull=True).exclude(
+        root_categories = models.Category.objects.filter(parent__isnull=True).exclude(
             title='سایر'
         )
         last_news = {}
         for category in root_categories:
-            posts = models.Post.objects.all() \
+            posts = models.Post.objects \
                         .filter(category__in=category.sub_categories.all(), main_image__isnull=False) \
                         .order_by('-date_posted')[:3]
             last_news[category] = posts
