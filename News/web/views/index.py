c07644311181e9b803a30790ce2f9464e78475e4
Hosein Alirezaee <hoseinalirezaee128@gmail.com>
2020-06-26 12:22:41 +0430

    add top news

--- News/web/views/index.py
+++ News/web/views/index.py
@@ -13,6 +13,24 @@ class Index(generic.TemplateView):
         context = super().get_context_data()
         context['last_news'] = last_news
         context['last_news_with_category'] = self.get_last_news_with_category()
+        top_news = self.get_top_news()
+        top_news_count = top_news.count()
+        context['top_new_left'] = top_news[top_news_count - 2:top_news_count]
+        context['top_news_slider'] = top_news.exclude(id__in=context['top_new_left'])
+        if self.request.user.is_authenticated:
+            agencies = self.request.user.favorite_agencies.all()
+            cats = self.request.user.favorite_categories.all()
+            categories = set()
+            for cat in cats:
+                if cat.parent is None:
+                    sub_cats = models.Category.objects.filter(parent=cat)
+                    categories = categories.union(set(sub_cats))
+                else:
+                    categories.add(cat)
+            recommended = models.Post.objects \
+                              .filter(agency__in=agencies, category__in=categories) \
+                              .order_by('-date_posted')[:8]
+            context['recommended'] = recommended
         return context
 
     def get_last_news(self):
@@ -32,3 +50,7 @@ class Index(generic.TemplateView):
             last_news[category] = posts
 
         return last_news
+
+    def get_top_news(self):
+        posts = models.TopPost.objects.all().order_by('-post__date_posted')
+        return posts
