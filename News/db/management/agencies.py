7551fe0a0c2862437ae5f8bc95d6444d45427999
Hosein Alirezaee <hoseinalirezaee128@gmail.com>
2020-06-27 22:08:55 +0430

    fix agencies url

--- News/db/management/agencies.py
+++ News/db/management/agencies.py
@@ -1,22 +1,22 @@
 AGENCIES = {
     'farsnews': {
         'title': 'خبرگزاری فارس',
-        'image': 'statics/img/fars.png'
+        'image': 'img/fars.png'
     },
     'irna': {
         'title': 'خبرگزاری ایرنا',
-        'image': 'statics/img/irna.png'
+        'image': 'img/irna.png'
     },
     'isna': {
         'title': 'خبرگزاری ایسنا',
-        'image': 'statics/img/isna.png'
+        'image': 'img/isna.png'
     },
     'mehrnews': {
         'title': 'خبرگزاری مهر',
-        'image': 'statics/img/mehr.jpg'
+        'image': 'img/mehr.jpg'
     },
     'tasnimnews': {
         'title': 'خبرگزاری تسنیم',
-        'image': 'statics/img/tasnim.png'
+        'image': 'img/tasnim.png'
     }
 }
