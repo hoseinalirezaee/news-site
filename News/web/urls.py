3edb930c5bd0cdcb305ec20d1ef31f0b85662e5d
Alireza Azadi <Alireza_Azadi@Hotmail.com>
2021-01-23 19:39:49 +0330

    using formatted url instead of using regex

--- News/web/urls.py
+++ News/web/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.Index.as_view()),
-    re_path(r'^posts/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='post-detail'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path('posts/', views.PostsListView.as_view(), name='post-list-view'),
     path('signup/success/', views.SignUpSuccessView.as_view(), name='signup_success'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
